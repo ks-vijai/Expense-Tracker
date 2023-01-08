8c3d1cb61f2177d4371afbea5ffb8a210dc1066f
ks-vijai <vijaiks.19cse@kongu.edu>
2023-01-08 08:48:33 +0530

    feat: expense-tracker: add balance component
    Signed-off-by: ks-vijai <vijaiks.19cse@kongu.edu>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Balance from "./components/Balance";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
+      <div className="expenses-list">
+        <Balance />
+      </div>
     </div>
   );
 }
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Balance() {
+  return (
+    <>
+      <div className="show-balance">Your Balance</div>
+      <div className="balance-amount">Rs 100.00</div>
+    </>
+  );
+}
+
+export default Balance;
