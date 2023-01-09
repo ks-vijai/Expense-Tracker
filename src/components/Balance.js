8d1f5daab36a76d2f989eca2444dda9f9c6ab1cb
ks-vijai <vijaiks.19cse@kongu.edu>
2023-01-09 06:06:05 +0530

    refactor: expense-tracker: display current balance amount
    Signed-off-by: ks-vijai <vijaiks.19cse@kongu.edu>

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 function Balance() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const totalAmount = amounts
+    .reduce((acc, amount) => acc + amount, 0)
+    .toFixed(2);
+
   return (
     <>
       <div className="show-balance">Your Balance</div>
-      <div className="balance-amount">Rs 100.00</div>
+      <div className="balance-amount">Rs {totalAmount}</div>
     </>
   );
 }
