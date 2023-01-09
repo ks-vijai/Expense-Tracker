6306c10d3eca5491d09540d4df84e6a7be26f23a
ks-vijai <vijaiks.19cse@kongu.edu>
2023-01-09 06:17:39 +0530

    refactor: expense-tracker: modify incomeexpenses
    Show accurate value of income and expense
    
    Signed-off-by: ks-vijai <vijaiks.19cse@kongu.edu>

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 function IncomeExpenses() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const incomeAmount = amounts
+    .filter((amount) => amount > 0)
+    .reduce((acc, amount) => acc + amount, 0)
+    .toFixed(2);
+
+  const expenseAmount = amounts
+    .filter((amount) => amount < 0)
+    .reduce((acc, amount) => acc + amount, 0)
+    .toFixed(2);
+
   return (
     <div className="income-expenses">
       <div>
         <div className="expenses-heading">INCOME</div>
-        <div className="income">Rs100.00</div>
+        <div className="income">Rs {incomeAmount}</div>
       </div>
       <div className="expenses-seperator"></div>
       <div>
         <div className="expenses-heading">EXPENSE</div>
-        <div className="expenses">Rs240.00</div>
+        <div className="expenses">Rs {expenseAmount}</div>
       </div>
     </div>
   );
